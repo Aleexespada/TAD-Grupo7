8683badcc7b55e6ef4d0bdd48b0b68e5b545be47
CarlosHerrera01 <76865831+CarlosHerrera01@users.noreply.github.com>
2023-04-09 19:08:32 +0200

    Añadidos roles al crear usuarios

--- my-project/app/Actions/Fortify/CreateNewUser.php
+++ my-project/app/Actions/Fortify/CreateNewUser.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Fortify;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -32,11 +33,16 @@ class CreateNewUser implements CreatesNewUsers
             'password' => $this->passwordRules(),
         ])->validate();
 
-        return User::create([
+        $user = User::create([
             'name' => $input['name'],
             'last_name' => $input['last_name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
+
+        $role = Role::where('name', 'user')->first();
+        $user->roles()->attach($role, ['created_at' => now(), 'updated_at' => now()]);
+
+        return $user;
     }
 }
--- my-project/app/Models/User.php
+++ my-project/app/Models/User.php
@@ -55,7 +55,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function roles()
     {
-        return $this->belongsToMany(Role::class);
+        return $this->belongsToMany(Role::class, 'user_role');
     }
 
     public function reviews()
