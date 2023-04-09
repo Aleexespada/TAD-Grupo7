6970cbde0516fb6b7861b568304cade52c8783fd
CarlosHerrera01 <76865831+CarlosHerrera01@users.noreply.github.com>
2023-04-09 19:11:08 +0200

    Implementada redireccion a la pagina en la que se cierra sesion

--- my-project/app/Providers/FortifyServiceProvider.php
+++ my-project/app/Providers/FortifyServiceProvider.php
@@ -10,6 +10,7 @@ use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Contracts\LogoutResponse;
 use Laravel\Fortify\Fortify;
 
 class FortifyServiceProvider extends ServiceProvider
@@ -19,7 +20,13 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->instance(LogoutResponse::class, new class implements LogoutResponse {
+            public function toResponse($request)
+            {
+                return redirect()->back();
+            }
+        });
+
     }
 
     /**
