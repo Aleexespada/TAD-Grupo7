e61dd8667c5d66944c24cf4ae599adb9b0702530
Antonio Manuel <anmamebo2001@gmail.com>
2023-04-21 01:06:27 +0200

    Cambiadas fuentes

--- my-project/resources/views/admin/partials/header.blade.php
+++ my-project/resources/views/admin/partials/header.blade.php
@@ -14,7 +14,7 @@
                 </button>
                 <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasAdmin" aria-labelledby="offcanvasProfileLabel">
                     <div class="offcanvas-header">
-                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Opciones</h5>
+                        <h5 class="offcanvas-title fw-bold" id="offcanvasNavbarLabel">Opciones</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
                     <div class="offcanvas-body">
